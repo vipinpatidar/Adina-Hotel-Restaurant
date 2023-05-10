703b0e2f0fda4f3cec2107ef508405600d487a59
vipin Patidar <vipinpatidar5@gmail.com>
2023-05-10 13:12:19 +0530

    changed padding

--- src/pages/Restaurant.js
+++ src/pages/Restaurant.js
@@ -110,7 +110,7 @@ const Restaurant = ({ length, isHome }) => {
             </h2>
           </div>
         )}
-        <div className="grid grid-cols-1 px-5 max-w-sm mx-auto gap-[30px] lg:grid-cols-3 lg:max-w-none lg:mx-0 my-16">
+        <div className="grid grid-cols-1 px-4 max-w-sm mx-auto gap-[30px] lg:grid-cols-3 lg:max-w-none lg:mx-0 my-16">
           {newResodata.map((resto) => (
             <RestoCard
               key={resto.id}
